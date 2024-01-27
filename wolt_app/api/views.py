52338ca296e037d77cec6054269987a5336e33c8
Carlos Orellana <corellana1988@gmail.com>
2024-01-27 09:20:46 +0200

    ongoing

--- wolt_app/api/views.py
+++ wolt_app/api/views.py
@@ -1,13 +1,55 @@
-from django.http import JsonResponse
+from datetime import datetime
+from dateutil import parser
+from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-def	isValidKeys(decodedBody):
-	validWords = ['cart_value', 'delivery_distance', 'number_of_items', 'time']
-	for key in decodedBody:
-		if not key in validWords:
-			return False
-	return True
+class JSONInputError(Exception):
+	pass
+
+class handleRequest():
+
+	def __isValidType(self):
+		integersList = [self.__cartValue, self.__deliveryDistance, self.__numberOfItems]
+		for integers in integersList:
+			if type(integers).__str__() != "<class 'int'>":
+				raise JSONInputError
+		if type(self.__time).__str__() != "<class 'str'>":
+			raise JSONInputError
+
+	def __isValidKeys(self):
+		validWords = ['cart_value', 'delivery_distance', 'number_of_items', 'time']
+		for key in self.__decodedBody:
+			if not key in validWords:
+				raise JSONInputError
+		try:
+			self.__cartValue = self.__decodedBody['cart_value']
+			self.__deliveryDistance = self.__decodedBody['delivery_distance']
+			self.__numberOfItems = self.__decodedBody['number_of_items']
+			self.__time = self.__decodedBody['time']
+		except KeyError:
+			raise JSONInputError
+
+	def __init__(self, request : HttpRequest):
+		self.__rawImput = ""
+		self.__decodedBody = {}
+		self.__cartValue = 0
+		self.__deliveryDistance = 0
+		self.__numberOfItems = 0
+		self.__time = ""
+		if request.headers.get('Content-Type') != 'application/json':
+			raise JSONInputError
+		self.__rawImput = request.body.decode()
+		try:
+			self.__decodedBody = json.loads(self.__rawImput)
+			self.__isValidKeys()
+			self.__isValidType()
+		except (json.JSONDecodeError, JSONInputError):
+			raise JSONInputError
+	
+	def __str__(self):
+		return "handleRequestClass"
+	
 
 @csrf_exempt #Removed the csrf protection to be able to send request through postman or thunderclient
 def	receiveRequest(request):
